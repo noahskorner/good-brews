880ed064e0e532646820990f29c16934a930d1d1
Noah Korner <noahskorner@gmail.com>
2022-01-16 14:23:08 -0600

    Added NODE_ENV.

--- server/models/index.js
+++ server/models/index.js
@@ -3,22 +3,23 @@ const dbConfig = require("../config/db.config.js");
 const Sequelize = require("sequelize");
 const initModels = require("./init-models");
 
-const sequelize = process.env.DEV_MODE
-  ? new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
-      host: dbConfig.host,
-      dialect: dbConfig.dialect,
-      logging: false,
-    })
-  : new Sequelize(dbConfig.connectionString, {
-      dialect: dbConfig.dialect,
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: dbConfig.rejectUnauthorized,
+const sequelize =
+  process.env.NODE_ENV !== "production"
+    ? new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
+        host: dbConfig.host,
+        dialect: dbConfig.dialect,
+        logging: false,
+      })
+    : new Sequelize(dbConfig.connectionString, {
+        dialect: dbConfig.dialect,
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: dbConfig.rejectUnauthorized,
+          },
         },
-      },
-      logging: false,
-    });
+        logging: false,
+      });
 
 const db = {};
 db.Sequelize = Sequelize;
