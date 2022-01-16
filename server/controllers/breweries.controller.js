de2d6b4ee9dac9c3a6ffac8dae9dbb768da3b7e8
Noah Korner <noahskorner@gmail.com>
2022-01-15 21:04:01 -0600

    Created getBrewery controller.

--- server/controllers/breweries.controller.js
+++ server/controllers/breweries.controller.js
@@ -25,6 +25,33 @@ const getBreweries = async (req, res) => {
   return res.status(200).json(breweries);
 };
 
+const getBrewery = async (req, res) => {
+  try {
+    const obdb_id = req.params.id;
+
+    if (!obdb_id) return res.sendStatus(400);
+
+    const brewery = await Breweries.findOne({
+      where: {
+        obdb_id,
+      },
+    })
+      .then((data) => data)
+      .catch((error) => {
+        console.log(error);
+        return res.sendStatus(500);
+      });
+
+    if (!brewery) return res.sendStatus(400);
+
+    return res.status(200).json(brewery);
+  } catch (error) {
+    console.log(error.message);
+    return res.sendStatus(500);
+  }
+};
+
 module.exports = {
   getBreweries,
+  getBrewery,
 };
